afd1367ef1cc28a423fac9343071a841f45bce0b
Ismail <shaikhnagismail@gmail.com>
2023-11-25 23:53:54 +0200

    Update page title styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+    <main className="">
+      <h1 className="text-xl">Home</h1>
     </main>
   );
 }
