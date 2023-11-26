68a60e1aa6e533b266d6db8091715a4fbaba2554
Ismail <shaikhnagismail@gmail.com>
2023-11-26 22:51:52 +0200

    Refactor AboutSection component

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,20 +1,17 @@
-
 import React from "react";
 import FadeInContent from "./FadeInContent";
 
-
 const AboutSection = () => {
   return (
-    <section className="master-container my-20 select-none overflow-hidden py-10 md:my-[12%]  ">
-      <FadeInContent >
-        <h2 
-        className="text-4xl font-medium md:text-5xl lg:text-6xl">
+    <div className="master-container flex h-screen select-none items-center justify-center overflow-hidden">
+      <FadeInContent>
+        <h2 className="text-4xl font-medium md:text-5xl lg:text-6xl">
           My passion for design, code, and interaction places me uniquely in the
-          web design world. I don&apos;t just create websites; I craft impactful,
-          immersive journeys.
+          web design world. I don&apos;t just create websites; I craft
+          impactful, immersive journeys.
         </h2>
-        </FadeInContent>
-    </section>
+      </FadeInContent>
+    </div>
   );
 };
 
