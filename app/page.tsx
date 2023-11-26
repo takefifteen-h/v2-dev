b6e042c03e906fe0bb7314009040ffd25dc3c487
Ismail <shaikhnagismail@gmail.com>
2023-11-26 06:46:26 +0200

    Remove unnecessary components from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import React from "react";
-
-import Navbar from "@/components/Navbar";
-import AnimatePageContent from "@/components/AnimatePageContent";
-
 export default function Home() {
   return (
-    <AnimatePageContent>
-      <React.Fragment>
-        <Navbar />
-        <main className="mt-[90px]">
-          <h1 className="text-xl">Home</h1>
-        </main>
-      </React.Fragment>
-    </AnimatePageContent>
+    <main className="mt-[90px]">
+      <h1 className="text-xl">Home</h1>
+    </main>
   );
 }
