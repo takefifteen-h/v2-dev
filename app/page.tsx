8c6d04409fbc80f106cefaac3aa3a5bce4406d3d
Ismail <shaikhnagismail@gmail.com>
2023-11-26 10:22:56 +0200

    Add lorem ipsum section to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main className="vh-100">
       <HeroSection />
+
+      {/* add a bunch of lorem ipsum */}
+      <div className="h-screen bg-black"></div>
+      
     </main>
   );
 }
