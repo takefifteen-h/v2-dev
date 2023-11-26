72b858ffde106b2e32026e2490978f915fc4d9fd
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-11-26 17:56:35 +0000

    new services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/heroSection/HeroSection";
 import AboutSection from "@/components/AboutSection";
+import ServiceSection from "@/components/ServiceSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <ServiceSection />
     </main>
   );
 }
