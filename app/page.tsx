82176fa5de6a18b756ebb322a560ee96ec1672cf
Ismail <shaikhnagismail@gmail.com>
2023-11-26 06:33:42 +0200

    Add Navbar and AnimatePageContent components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+
+import Navbar from "@/components/Navbar";
+import AnimatePageContent from "@/components/AnimatePageContent";
+
 export default function Home() {
   return (
-    <main className="mt-[90px]">
-      <h1 className="text-xl">Home</h1>
-    </main>
+    <AnimatePageContent>
+      <React.Fragment>
+        <Navbar />
+        <main className="mt-[90px]">
+          <h1 className="text-xl">Home</h1>
+        </main>
+      </React.Fragment>
+    </AnimatePageContent>
   );
 }
