b92a03986e2b502b7baf23e4ab239a9db395113c
Ismail <shaikhnagismail@gmail.com>
2023-11-26 17:18:36 +0200

    Add AboutSection component

---
+++ components/AboutSection.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <section className="master-container my-20 flex select-none flex-col gap-6 overflow-hidden py-10 md:my-[12%] md:flex-row md:items-center ">
+      <h2 className="text-4xl font-medium md:text-5xl lg:text-6xl">
+        Crafting digital experiences that captivate and resonate.
+      </h2>
+
+      <p className="max-w-[32rem] text-base md:text-lg lg:text-xl">
+        My passion for design, code, and interaction places me uniquely in the
+        web design world. I don&apos;t just create websites; I craft impactful,
+        immersive journeys.
+      </p>
+    </section>
+  );
+};
+
+export default AboutSection;
