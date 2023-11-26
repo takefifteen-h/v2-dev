e6525f3ae16f809392d48b63a26fa3e47051d8fc
Ismail <shaikhnagismail@gmail.com>
2023-11-26 09:28:19 +0200

    Update navbar height in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       height: {
-        navbar: "var(--navbar-height)",
+        "navbar-desktop": "85px",
+        "navbar-mobile": "70px",
       },
     },
   },
