d3676240c19c7d5349abbbc3fb525eefbeff694c
Ismail <shaikhnagismail@gmail.com>
2023-11-28 01:58:25 +0200

    Add Cozy Cinema project to constants/index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -6,6 +6,8 @@ const crispycanvas_mp4 =
   "videos/project-walkthoughs/mp4/crispycanvas_walkthrough.mp4";
 const globetracer_mp4 =
   "videos/project-walkthoughs/mp4/globetracer_walkthrough.mp4";
+const cozycinema_mp4 =
+  "videos/project-walkthoughs/mp4/cozycinema_walkthrough.mp4";
 const serengeti_webm =
   "videos/project-walkthoughs/webm/serengeti_walkthrough.webm";
 const crispycanvas_webm =
@@ -17,14 +19,82 @@ export type Project = {
   projectTitle: string;
   description: string;
   tags: Array<{ name: string }>;
-  mp4: any;
-  webm: any;
+  mp4: string;
+  webm: string | undefined;
   source_code_link: string;
   live_site_link?: string;
-  github_repo_link: string;
 };
 
 export const projects: Project[] = [
+  {
+    projectTitle: "Cozy Cinema",
+    description:
+      "Dive into a world of entertainment with our advanced Next.js web app. Effortlessly authenticated via NextAuth, relish free streaming of premium movies and TV series from Netflix, Hulu, HBO Max, Apple TV, Amazon Prime, and beyond. Explore functionalities such as adding shows to your library and monitoring your watch history. All presented in a contemporary, streamlined design for an immersive entertainment experience. 🍿🎥",
+    tags: [
+      {
+        name: "nextjs",
+      },
+      {
+        name: "typescript",
+      },
+      {
+        name: "tailwind",
+      },
+      {
+        name: "mongodb",
+      },
+      {
+        name: "nextauth",
+      },
+      {
+        name: "tmdb api",
+      },
+    ],
+    mp4: cozycinema_mp4,
+    webm: undefined,
+    source_code_link: "https://github.com/PresidentIsmail/CozyCinema",
+    live_site_link: "https://cozycinema.vercel.app/",
+  },
+
+  {
+    projectTitle: "Crispy Canvas",
+    description:
+      "A React ordering application that lets you order your favourite pizzas while effortlessly managing orders and accessing order history. With my expertise, I crafted a seamless user experience that tantalizes taste buds and showcases my skills in front-end development. 🍕🎨",
+    tags: [
+      {
+        name: "react",
+      },
+      {
+        name: "restapi",
+      },
+      {
+        name: "css modules",
+      },
+    ],
+    mp4: crispycanvas_mp4,
+    webm: crispycanvas_webm,
+    source_code_link: "https://github.com/PresidentIsmail/Crispy-Canvas",
+    live_site_link: "https://crispy-canvas.netlify.app/",
+  },
+  {
+    projectTitle: "Globe Tracer",
+    description:
+      "Globe Tracer is a web application that allows users to track all the places they have traveled to around the world. With an interactive map and a user-friendly interface, users can easily record their travel experiences and view them at any time.",
+    tags: [
+      {
+        name: "React",
+      },
+      {
+        name: "Jsonserver",
+      },
+      {
+        name: "Mapbox",
+      },
+    ],
+    mp4: globetracer_mp4,
+    webm: globetracer_webm,
+    source_code_link: "https://github.com/PresidentIsmail/Globe-Tracer",
+  },
   {
     projectTitle: "Serengeti Sky Lodges",
     description:
@@ -47,50 +117,5 @@ export const projects: Project[] = [
     webm: serengeti_webm,
     source_code_link: "https://github.com/PresidentIsmail/Serengeti-Sky-Lodges",
     live_site_link: "https://serengeti-sky-lodges.vercel.app/",
-    github_repo_link: "https://github.com/PresidentIsmail"
-  },
-  {
-    projectTitle: "Crispy Canvas",
-    description:
-      "A React ordering application that lets you order your favourite pizzas while effortlessly managing orders and accessing order history. With my expertise, I crafted a seamless user experience that tantalizes taste buds and showcases my skills in front-end development. 🍕🎨",
-    tags: [
-      {
-        name: "react",
-      },
-      {
-        name: "restapi",
-      },
-      {
-        name: "css",
-      },
-    ],
-    mp4: crispycanvas_mp4,
-    webm: crispycanvas_webm,
-    source_code_link: "https://github.com/PresidentIsmail/Crispy-Canvas",
-    live_site_link: "https://crispy-canvas.netlify.app/",
-    github_repo_link: "https://github.com/PresidentIsmail"
-  },
-  {
-    projectTitle: "Globe Tracer",
-    description:
-      "Globe Tracer is a web application that allows users to track all the places they have traveled to around the world. With an interactive map and a user-friendly interface, users can easily record their travel experiences and view them at any time.",
-    tags: [
-      {
-        name: "React",
-      },
-      {
-        name: "Jsonserver",
-      },
-      {
-        name: "css",
-      },
-      {
-        name: "Mapbox",
-      },
-    ],
-    mp4: globetracer_mp4,
-    webm: globetracer_webm,
-    source_code_link: "https://github.com/PresidentIsmail/Globe-Tracer",
-    github_repo_link: "https://github.com/PresidentIsmail"
   },
 ];
