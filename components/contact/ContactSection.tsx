04c12c5e785c49b1580680a4f9469a8b964bca52
Ismail <shaikhnagismail@gmail.com>
2023-11-29 21:54:29 +0200

    Refactor contact section layout

--- components/contact/ContactSection.tsx
+++ components/contact/ContactSection.tsx
@@ -10,17 +10,16 @@ import SlideInContent from "../SlideInContent";
 const ContactSection = () => {
   return (
     <section className="master-container py-[64px]">
-
-<FadeInContent>
-<header className="flex w-full select-none items-start justify-start gap-x-2 pb-[calc(clamp(5rem,21vh,12rem)*.475)]">
+      <FadeInContent>
+        <header className="flex w-full select-none items-start justify-start gap-x-2 pb-[calc(clamp(5rem,21vh,12rem)*.475)]">
           {/* icon */}
-          <div className="relative translate-y-2 h-[30px] w-[30px] md:h-[48px] md:w-[48px] lg:h-[56px] lg:w-[56px]">
+          <div className="relative h-[30px] w-[30px] translate-y-2 md:h-[48px] md:w-[48px] lg:h-[56px] lg:w-[56px]">
             <Image src={projectsImg} alt="projects icon" fill />
           </div>
-      <h2 className=" font-bold text-[calc(clamp(3.25em,7vw,8em)*0.875)] leading-none">
-        Let&apos;s work <br/>  together
-      </h2>
-      </header>
+          <h2 className=" text-[calc(clamp(3.25em,7vw,8em)*0.875)] font-bold leading-none">
+            Let&apos;s work <br /> together
+          </h2>
+        </header>
       </FadeInContent>
 
       <div className="relative">
@@ -32,12 +31,12 @@ const ContactSection = () => {
         />
       </div>
 
-<SlideInContent delay={1}>
-      <ContactEmailLink
-        label="shaikhnagismail@gmail.com"
-        href="mailto:shaikhnagismail@gmail.com"
-        aria-label="email"
-      />
+      <SlideInContent delay={1}>
+        <ContactEmailLink
+          label="shaikhnagismail@gmail.com"
+          href="mailto:shaikhnagismail@gmail.com"
+          aria-label="email"
+        />
       </SlideInContent>
     </section>
   );
