01683d77164a24d459bd1401e2d48136389f577b
takefifteen <149362230+takefifteen@users.noreply.github.com>
2023-12-02 20:17:39 +0000

    Refactor VideoCanvas component and fix event listener memory leak

--- components/hero/VideoCanvas.tsx
+++ components/hero/VideoCanvas.tsx
@@ -4,15 +4,15 @@ import { useWindowWidth } from "@/hooks/useWindowWidth";
 import React, { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 
-import mobilePoster from "../../public/images/video-posters/hero-mobile-poster.webp"
-import desktopPoster from "../../public/images/video-posters/hero-desktop-poster.webp"
+import mobilePoster from "../../public/images/video-posters/hero-mobile-poster.webp";
+import desktopPoster from "../../public/images/video-posters/hero-desktop-poster.webp";
 
 const VideoCanvas = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [videoLoaded, setVideoLoaded] = useState(false);
   const windowWidth = useWindowWidth();
   const isMobile = windowWidth ? windowWidth < 768 : false;
-  
+
   const videoSource = isMobile
     ? "/videos/bg-mobile.mp4"
     : "/videos/bg-desktop.mp4";
@@ -22,27 +22,26 @@ const VideoCanvas = () => {
     ? "/images/video-posters/hero-mobile-poster.webp"
     : "/images/video-posters/hero-desktop-poster.webp";
 
-     useEffect(() => {
-  // Create a variable to hold the ref value
-  const currentVideoRef = videoRef.current;
+  useEffect(() => {
+    // Create a variable to hold the ref value
+    const currentVideoRef = videoRef.current;
 
-  if (currentVideoRef) {
-    // Listen for the 'loadeddata' event to know when the video has loaded
-    currentVideoRef.addEventListener("loadeddata", () => {
-      setVideoLoaded(true);
-    });
-  }
-
-  // Clean up event listener on component unmount
-  return () => {
     if (currentVideoRef) {
-      currentVideoRef.removeEventListener("loadeddata", () => {});
+      // Listen for the 'loadeddata' event to know when the video has loaded
+      currentVideoRef.addEventListener("loadeddata", () => {
+        setVideoLoaded(true);
+      });
     }
-  };
-}, []);
 
+    // Clean up event listener on component unmount
+    return () => {
+      if (currentVideoRef) {
+        currentVideoRef.removeEventListener("loadeddata", () => {});
+      }
+    };
+  }, []);
 
-   return (
+  return (
     <>
       {/* Placeholder image until video is loaded */}
       {!videoLoaded && (
@@ -71,10 +70,9 @@ const VideoCanvas = () => {
       </video>
 
       {/* Overlay for the video */}
-      <div className="absolute h-full w-full bg-black bg-opacity-40 md:bg-opacity-20"></div>
+      <div className="absolute h-full w-full bg-black bg-opacity-70 md:bg-opacity-65"></div>
     </>
   );
 };
-
 
 export default VideoCanvas;
